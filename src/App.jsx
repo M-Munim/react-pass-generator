77140d100ee7c2ce55899098c438ad5fc129a1b2
munim <muhammadmunimoff330@gmail.com>
2023-10-04 11:08:03 +0500

    phase 1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState, useCallback } from 'react'
-import './App.css'
+import { useState, useCallback, useEffect, useRefk, useRef } from 'react'
+// import './App.css'
 
 function App() {
   // setting length of bar
@@ -14,7 +14,10 @@ function App() {
   // setting main password field
   const [password, setPassword] = useState("");
 
-  // passgenerator method will callback itself on every change
+  // useRef Hook 
+  const passRef = useRef(null)
+
+  // passgenerator method will callback itself on every change also memorize(cache) and optimize
   const passGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -27,18 +30,36 @@ function App() {
       str += "!@#$%^&*()_+[]{}~`"
     }
 
-    for (let i = 1; i <= array.length; i++) {
+    for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
-      pass = str.charAt(char);
+      pass += str.charAt(char);
     }
 
     setPassword(pass)
-  }, [length, numberAllowed, charAllowed, setPassword])
+  },
+    // Mera method agar run hota hy again to usko optimize kro (cache my memorize rakho)
+    [length, numberAllowed, charAllowed, setPassword])
+
+  // for coping to clipboard, here our function will run
+
+  const copyToClipboard = useCallback(() => {
+    passRef.current?.select();
+    passRef.current?.setSelectionRange(0, 20)
+    window.navigator.clipboard.writeText(password)
+    // alert("copied")
+  }, [password])
+
+  // using useEffect so our app runs on every change
+  useEffect(() => {
+    passGenerator();
+  },
+    // agar inmy kui chair char ho to run ho jai
+    [length, numberAllowed, charAllowed, passGenerator])
 
 
   return (
     <>
-      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-4 my-8 text-orange-500 bg-gray-700">
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-4 my-48 text-orange-500 bg-gray-700">
 
         <h1 className='text-white text-2xl font-bold p-3 text-center'>Password Generator</h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-6">
@@ -48,8 +69,9 @@ function App() {
             className='outline-none w-full py-1 px-3'
             placeholder='Password'
             readOnly
+            ref={passRef}
           />
-          <button className='outline-none bg-blue-700 px-3 py-1 shrink-0 font-bold'>Copy</button>
+          <button className='outline-none bg-blue-700 text-white px-3 py-2 shrink-0 font-bold' onClick={copyToClipboard}>Copy</button>
         </div>
 
         <div className="flex text-sm gap-x-2">
@@ -61,7 +83,9 @@ function App() {
               value={length}
               className='cursor-pointer'
               // onchange length will be set according to the value
-              onChange={(e) => { setLength(e.target.value) }} />
+              onChange={(e) => { setLength(e.target.value) }} 
+
+              />
 
             <label htmlFor=""> length: {length}</label>
           </div>
